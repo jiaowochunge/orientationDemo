8050d70a2df1ab60e803aff549bbc9146bca79ae
王益 <wangyi@wangyideMac-mini.local>
2015-03-17 14:13:42 +0800

    Initial Commit

---
+++ testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by 王益 on 15/3/17.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
